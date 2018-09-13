a2950ef5faf5abf2f45a788d0277feee0af18766
javi <javicast3@gmail.com>
2018-09-13 16:41:08 -0300

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -17,9 +17,17 @@ app.post('/todos', (req, res) =>{
   todo.save().then((doc) => {
      res.send(doc);
   }, (e) => {
-    res.status().send(e);
+    res.status(400).send(e);
   });
 
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e)
+  });
 });
 app.listen(3000, () => {
   console.log('Started on port 3000');
