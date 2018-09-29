65fa48e5eb41b23728316efdee6fbfcea5fe37d2
javi <javicast3@gmail.com>
2018-09-29 15:51:01 -0300

    test PATCH /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'Firs test todo'
 },{
   _id: new ObjectID(),
-  text:'Second test todo'
+  text:'Second test todo',
+  completed: true,
+  completedAt: 333
 }]
 
 //delete any document in Todo collection in database
@@ -144,6 +146,46 @@ describe('GET /todos/:id', () => {
     });
 
   });
+describe('PATCH /todos/:id', () => {
 
+  it('Should update the todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+      })
+      .end(done);
+
+  });
+
+  it('Should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+      })
+      .end(done);
+
+  });
+
+});
 
 });
