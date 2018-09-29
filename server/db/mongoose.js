fa6af7f7233a9c91e591343903501f6cfcd18533
javi <javicast3@gmail.com>
2018-09-29 16:30:02 -0300

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 const monguri='mongodb://javicast:javi1750@ds135441.mlab.com:35441/mongonodeapi'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(monguri || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(monguri || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
