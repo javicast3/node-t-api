49a6bc74d4aa05d64286abdbe9c12cba7c793915
javi <javicast3@gmail.com>
2018-10-10 19:19:06 -0300

    Delete /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -41,6 +41,15 @@ UserSchema.methods.toJSON = function(){
   return _.pick(userObject, ['_id', 'email']);
 }
 
+UserSchema.methods.removeToken = function (token){
+  var user = this;
+ return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
