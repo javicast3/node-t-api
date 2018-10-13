c38168ab8f1149eefbab1cf4feeb9197caf9a863
javi <javicast3@gmail.com>
2018-10-13 10:39:46 -0300

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -53,7 +53,7 @@ UserSchema.methods.removeToken = function (token){
 UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id:user._id.toHexString(), access},'abc123').toString();
+  var token = jwt.sign({_id:user._id.toHexString(), access},process.env.JWT_SECRET).toString();
   user.tokens = user.tokens.concat([{access, token}]);
 
   return user.save().then(() => {
